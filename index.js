6e13968aa27fa2f5e2647303db349c7e1e59dfaf
amin <sayedamin01@gmail.com>
2023-04-10 13:04:42 +0530

    3commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
+};
+
+async function getSeats() {
+  const connection = await mysql.createConnection(dbConfig);
+  const [rows] = await connection.execute('SELECT * FROM seats');
+  connection.end();
+  return rows;
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/seats', async (req, res) => {
+  try {
+    const seats = await getSeats();
+    res.json(seats);
+  } catch (err) {
+    console.error('Error querying database: ', err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
