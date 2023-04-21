3bc2e675bff9e18a70c39c13acb20ba313823781
amin <sayedamin01@gmail.com>
2023-04-21 17:55:02 +0530

    add databse functionlity

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ async function getSeats() {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
+
 
 app.get('/seats', async (req, res) => {
   try {
@@ -36,6 +38,31 @@ app.get('/seats', async (req, res) => {
     res.sendStatus(500);
   }
 });
+app.post('/seats/:seatNumber/reserve', async (req, res) => {
+  const { seatNumber } = req.params;
+
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.execute('UPDATE seats SET is_available = 0 WHERE seat_number = ?', [seatNumber]);
+    connection.end();
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(`Error reserving seat ${seatNumber}: `, err);
+    res.sendStatus(500);
+  }
+});
+app.put('/seats/clear', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.execute('UPDATE seats SET is_available = 1');
+    connection.end();
+    res.sendStatus(200);
+  } catch (err) {
+    console.error('Error updating seats: ', err);
+    res.sendStatus(500);
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
